353439d7cbb8523431af520f12014e7868187f55
HyunjoonShim <dokilove2@gmail.com>
2019-08-20 15:25:42 +0900

    언리얼 기본 세팅 + 입력

---
+++ Source/testVRcpp/testVRcppGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "testVRcppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTVRCPP_API AtestVRcppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
