7121f75def96e902878eaebf9a440548884b26ef
dokilove <dokilove2@gmail.com>
2019-08-21 00:43:29 +0900

    cpp 파일 생성및 블루프린트 생성

---
+++ Source/testVRcpp/MyMotionControllerPawn.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the D escription page of Project Settings.
+
+
+#include "MyMotionControllerPawn.h"
+#include "Runtime/Engine/Classes/Components/SceneComponent.h"
+#include "Runtime/Engine/Classes/Camera/CameraComponent.h"
+#include "Runtime/HeadMountedDisplay/Public/HeadMountedDisplayFunctionLibrary.h"
+
+// Sets default values
+AMyMotionControllerPawn::AMyMotionControllerPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	VROrigin = CreateDefaultSubobject<USceneComponent>(TEXT("VROrigin"));
+	VROrigin->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(VROrigin);
+
+	UE_LOG(LogTemp, Warning, TEXT("HMD Name "));
+}
+
+// Called when the game starts or when spawned
+void AMyMotionControllerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyMotionControllerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyMotionControllerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
